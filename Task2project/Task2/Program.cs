5e689d192f81084ecd9157f640a8272169398e58
Gentlestack <valera_dyshlovoy@mail.ru>
2023-12-29 05:09:31 +0400

    Исправила вторую задачу

--- Task2project/Task2/Program.cs
+++ Task2project/Task2/Program.cs
@@ -6,13 +6,20 @@ int[,] mass = new int[3, 4]
     {7, 6, 9, 10}
 };
  
+int buffer; 
 
+for (int j = 0; j < mass.GetLength(1); j++)
+{
+    buffer = mass[0, j];
+    mass[0, j] = mass[mass.GetLength(0) - 1, j];
+    mass[mass.GetLength(0) - 1, j] = buffer;
+}
 
 for (int i = 0; i < mass.GetLength(0); i++)
 {
     for (int j = 0; j < mass.GetLength(1); j++)
     {
-        Console.Write(mass[i, j]+ " ");
+        Console.Write(mass[i, j] + " ");
     }
     Console.WriteLine();
 }
