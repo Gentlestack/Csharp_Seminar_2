adfb845263826c2de172abb560799664df8eff2a
Gentlestack <valera_dyshlovoy@mail.ru>
2023-12-29 04:58:49 +0400

    Исправила вторую задачу

--- Task2project/Task2/Program.cs
+++ Task2project/Task2/Program.cs
@@ -1,27 +1,16 @@
 ﻿
-int[,] mass = new int[4, 4]
+int[,] mass = new int[3, 4]
 {
-    {5,9,3,1 },
-    {2,3,0,8 },
-    {7,6,9,10 },
-    {0,1,2,4 }
+    {5, 9, 3, 1},
+    {2, 3, 0, 8},
+    {7, 6, 9, 10}
 };
  
-int bufer;
- 
-            
-for (int j = 0; j < 4; j++)
-{
-    bufer = mass[0, j];
-    mass[0, j] = mass[mass.GetLength(1)-1,j] ;
-    mass[mass.GetLength(1)-1,j] = bufer;
- 
-}
-            
 
-for (int i = 0; i < 4; i++)
+
+for (int i = 0; i < mass.GetLength(0); i++)
 {
-    for (int j = 0; j < 4; j++)
+    for (int j = 0; j < mass.GetLength(1); j++)
     {
         Console.Write(mass[i, j]+ " ");
     }
